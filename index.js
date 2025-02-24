8e8e80f4f0f62fffacf9ec6f371e9e7427ad335b
Narasing <murapakanarasingarao02@gmail.com>
2025-02-24 22:05:26 +0530

    first deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express =require('express');
 require('dotenv').config();
 const cors = require('cors');
 const app =express();
+const { alldl } = require('rahad-all-downloader');
 
 const PORT = process.env.PORT || 5000;
 app.use(cors());
@@ -10,6 +11,28 @@ app.get('/',(req,res)=>{
     res.send('Hello World');
 })
 
+app.get('/download', async (req, res) => {
+    const url = req.query.url;
+
+    // Check if URL is missing from query
+    if (!url) {
+        return res.status(400).json({ error: 'Missing URL' });
+    }
+
+    try {
+        // Attempt to process the download with 'alldl'
+        const result = await alldl(url);
+        console.log(result); // Debugging: Log the result
+
+        // Send back the result as JSON
+        res.json(result);
+    } catch (error) {
+        console.error('AllDL Error:', error); // Log the error
+
+        // Return a 500 status with a generic error message
+        return res.status(500).json({ error: 'Failed to process video' });
+    }
+});
 
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}` );
